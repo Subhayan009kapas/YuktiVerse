edf0b24ceb879502ca8da3936c04f6b3279448b7
Subhayan009kapas <subhayankapas47@gmail.com>
2025-08-14 21:14:11 +0530

    delopeing time fixing 2

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,21 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
-const connectDB=async()=>{
-
-          try{
-                    const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}`)
-
-                    console.log(`MongoDB is connected DBHOST !! ${connectionInstance.connection.host}`)
-          }
-          catch(error){
-                    console.log("Mongodb connetion error !!" );
-                    process.exit(1)
-          }
-          
-
-}
+    console.log(
+      `MongoDB is connected DBHOST !! ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Mongodb connetion error !!");
+    process.exit(1);
+  }
+};
 export default connectDB;
